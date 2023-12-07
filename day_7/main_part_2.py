8a1c671d41496c84ef2eb259446922d51038f634
Romain Goussault <romain.goussault@owkin.com>
2023-12-07 11:56:38 +0100

    up day 7
    Signed-off-by: Romain Goussault <romain.goussault@owkin.com>

--- day_7/main_part_2.py
+++ day_7/main_part_2.py
@@ -2,14 +2,14 @@ import numpy as np
 from collections import Counter
 from functools import cmp_to_key
 
-filename = "input_test.csv"
+filename = "input.csv"
 
 with open(filename, 'r') as file:
     lines = file.readlines()
 
 # Define a mapping function
-def transform_element(element):
-    mapping = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}
+def cards_letter_to_int(element):
+    mapping = {'A': 14, 'K': 13, 'Q': 12, 'J': 1, 'T': 10}
 
     # Apply the mapping function
     return int(mapping.get(element, element))  # Use the element itself if not found in the mapping
@@ -17,9 +17,32 @@ def transform_element(element):
 def hand_to_list_of_int(hand):
 
     hand_list = list(hand)
-    transformed_list = [transform_element(item) for item in hand_list]
+    transformed_list = [cards_letter_to_int(item) for item in hand_list]
 
     return transformed_list
+
+def get_first_score(hand):
+
+    hand_list = list(hand)
+    joker_counts = hand_list.count("X")
+    # print("joker_counts", joker_counts)
+
+    if joker_counts == 0:
+        return get_score(hand)
+
+    joker_idx = [index for index, value in enumerate(hand_list) if value == "X"]
+
+    score = 0
+
+    for idx in joker_idx:
+
+        for replacement_value in ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]:
+            hand_list[idx] = replacement_value
+            hand_str = ''.join(hand_list)
+            new_score = get_first_score(hand_str)
+            score = max(score, new_score)
+
+    return score
 
 def get_score(hand):
 
@@ -28,10 +51,6 @@ def get_score(hand):
     result = Counter(hand_list)
 
     counts_most_common = result.most_common(1)[0][1]
-    value_most_common = result.most_common(1)[0][0]
-
-    # print("counts_most_common", counts_most_common)
-    # print("value_most_common", value_most_common)
 
     # hand score
     is_five_of_a_kind = len(set(hand_list)) == 1
@@ -40,9 +59,6 @@ def get_score(hand):
         # print("Five of a kind")
         # score = 1000 + hand_list[0]
         return 1000
-
-    counts_second_most_common = result.most_common(2)[1][1]
-    # print("counts_second_most_common", counts_most_common)
 
     is_four_of_a_kind = counts_most_common == 4
 
@@ -90,9 +106,12 @@ def get_second_score(hand):
 # Define a custom comparison function
 def custom_compare(hand_x, hand_y):
 
+    hand_x_XX= hand_x.replace("J", "X")
+    hand_y_XX = hand_y.replace("J", "X")
+
     # Compare elements based on their custom order
-    hand_x_score = get_score(hand_x)
-    hand_y_score = get_score(hand_y)
+    hand_x_score = get_first_score(hand_x_XX)
+    hand_y_score = get_first_score(hand_y_XX)
 
     if hand_x_score == hand_y_score:
         hand_x_score = get_second_score(hand_x)
@@ -102,26 +121,27 @@ def custom_compare(hand_x, hand_y):
 
 
 hand_str_list = []
-dict_machine = {}
+bid_dict = {}
 
 for id, line in enumerate(lines):
+    # print("progress: ", id, "/", len(lines))
     hand_str = line.split()[0]
-    bid = int(line.split()[1])
-    # print(hand_str, bid)
-    hand_str_list.append(hand_str)
-    dict_machine[hand_str] = bid
 
+    bid = int(line.split()[1])
+    hand_str_list.append(hand_str)
+    bid_dict[hand_str] = bid
+
+print("Sorting start")
 # Sort the list based on the custom comparison function
 sorted_hands = sorted(hand_str_list, key=cmp_to_key(custom_compare))
-# print(sorted_hands)
-
+print("Sorting done")
 
 # final stuff
 sum = 0
 for i, hand in enumerate(sorted_hands):
 
     rank = i + 1
-    bid = dict_machine[hand]
+    bid = bid_dict[hand]
     sum += bid * rank
 
 print(sum)
